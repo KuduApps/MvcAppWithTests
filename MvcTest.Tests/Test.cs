b0d48d2b647f80cf909b75a37e0ed92d5cc42e60
Amit Apple <amitap@microsoft.com>
2013-04-11 17:23:16 -0700

    MVCTest

---
+++ MvcTest.Tests/Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcTest.Controllers;
+
+namespace MvcTest.Tests
+{
+    [TestClass]
+    public class HomeTests
+    {
+        [TestMethod]
+        public void TestHome()
+        {
+            var h = new HomeController();
+            h.ToString();
+        }
+
+        [TestMethod]
+        public void TestHomeFail()
+        {
+            Assert.Fail("This fails");
+        }
+    }
+}
